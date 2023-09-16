8e822526fb56147140939493974bc31a3a9cfc23
natalia fernandez <83042545+Nat795@users.noreply.github.com>
2023-09-16 11:50:29 -0300

    varios

--- README.md
+++
@@ -1,2 +0,0 @@
-# Tienda_de_productos
-Una tienda que muestra productos de su comercio en venta
---
+++ js/script.js
@@ -0,0 +1,37 @@
+/*
+var palabra = prompt("ingrese una palabra")
+for(i=0; i < 10 ; i ++){
+    console.log(palabra + " " + i );
+}
+var numPositivo = Number(prompt("ingrese un numero entero positivo"))
+for(i =0; numPositivo%2 != 0; i++){
+    if (numPositivo%2 == 0 && Number.isInteger(numPositivo) && numPositivo > 0 ){
+        for (let i =0 ; i <= numPositivo;i++){
+            if(i%2 != 0 ){
+                console.log(i);
+            }
+        }
+    }
+    else{
+        prompt("El numero ingresado no es el correcto")
+    }
+    break
+}
+
+var flag = true;
+while(flag){
+    var numPositivo = Number(prompt("ingrese un numero entero positivo"))
+
+    if (numPositivo%2 == 0 && Number.isInteger(numPositivo) && numPositivo > 0 ){
+        flag = false;
+        for (let i =0 ; i <= numPositivo;i++){
+            if(i%2 != 0 ){
+                console.log(i);
+            }
+        }
+    }
+    else{
+        prompt("El numero ingresado no es el correcto")
+    }
+
+}*/
